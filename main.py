27e28d6078eff18aa2b9bfe984df1fde1e924c52
Geoffroy Penny <geoffroypenny@hotmail.com>
2019-02-20 11:08:06 -0500

    updated notebook

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from treeviz import tree_print
 from sklearn.metrics import accuracy_score
 
 
+tennis_df = pd.read_csv('tennis_numeric.csv')
+tennis_df
+
+tennis_dtree = tree.DecisionTreeClassifier(criterion='entropy')
+#tennis_dtree.fit(features_df)
